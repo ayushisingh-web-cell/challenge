67162480e27698f2d9dd12bc08547c44c2c5b7fe
Ayushi Singh Bhadauria <72434707+ayushisingh-web-cell@users.noreply.github.com>
2020-10-07 14:10:02 +0530

    uploded file from codeblocks
    it was a challenge given by my captain

---
+++ challenge.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+    printf("enter the marks:");
+    int marks=0;
+    scanf("%d",&marks);
+
+    if(marks>85)
+    {
+        printf("Grade A");
+    }
+    else if(marks>70&& marks<85)//%%,//,!
+    {
+        printf("Grade B");
+    }
+    else if(marks>55&& marks<70)
+    {
+        printf("Grade C");
+    }
+    else if(marks>40&& marks<55)
+    {
+        printf("Grade D");
+    }
+    else if(marks<40)
+    {
+        printf("Grade F");
+    }
+}
